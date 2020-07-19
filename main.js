7b98c15732f4393acf8d4a95d4bc009e3bd568a5
MaryMaina98 <52330368+MaryMaina98@users.noreply.github.com>
2020-07-19 12:53:12 +0300

    assessment01

---
+++ main.js
@@ -0,0 +1,15 @@
+//Question 3
+
+console.log(0.1 + 0.2);//0
+console.log((0.1+0.2) == 0.3); //false
+console.log( (0.1 + 0.2) === 0.3); //false
+
+
+// Question 4
+console.log(NaN !== NaN); //True
+
+//Question 5
+console.log("0 || 1 = " +(0 || 1)); // 0 || 1 = 1
+console.log("1 || 2 = " +(1 || 2)); // 1 || 2 = 1
+console.log("0 && 1 = " +(0 && 1)); // 0 && 1 = 0
+console.log("1 && 2 = " +(1 && 2)); // 1 && 2 = 2
